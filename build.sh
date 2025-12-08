0548527335117c29f780c5c46726d76e322bb0cd
Ivan T. Ivanov <iivanov@suse.de>
2025-12-08 16:44:38 +0200

    Add build shell script

---
+++ build.sh
@@ -0,0 +1,17 @@
+
+./models.py
+./districts.py
+./municipalities.py
+./locations.py
+
+./details.py
+./finance.py
+./transform.py
+./institutions.py
+
+./subjects.py
+./scores.py
+
+./religion.py
+./language.py
+./ethnicity.py
